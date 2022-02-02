8366ec2b81ca1f57104c532e4df54384a4629e81
NateWalchenbach <natewalchenbach24@gmail.com>
2022-02-01 22:30:47 -0800

    created quesions and swich case

--- server.js
+++ server.js
@@ -1,3 +1,47 @@
 const inquirer = require("inquirer");
 const mySql2 = require("mysql2");
 const consoleTable = require("console.table");
+
+// Questions to prompt user for
+
+const questionInt = [
+  {
+    type: "list",
+    message: "What would you like do today?",
+    choices: [
+      "View all employees",
+      "Add Employee",
+      "Update employee role",
+      "View all roles",
+      "Add role",
+      "View all departments",
+      "Add Departments",
+    ],
+  },
+];
+
+inquirer.prompt(questionInt).then((res) => {
+  switch (res.questionInt) {
+    case "View all employees":
+      viewAllEmployees();
+      break;
+    case "Add Employee":
+      addEmployee();
+      break;
+    case "Update employee role":
+      updateRole();
+      break;
+    case "View all roles":
+      viewAllRoles();
+      break;
+    case "Add role":
+      addRole();
+      break;
+    case "View all departments":
+      viewAllDepartments();
+      break;
+    case "Add Departments":
+      addDepartment();
+      break;
+  }
+});
