cc41de8ffbfb2b9198d22d17028cbdf79198a861
NateWalchenbach <natewalchenbach24@gmail.com>
2022-02-04 21:57:44 -0800

    changed mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,33 @@
-CREATE DATABASE database;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
-CREATE TABLE IF NOT EXISTS department (
-id INT NOT NULL PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
-CREATE TABLE IF NOT EXSITS role(
-    id INT NOT NULL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id),
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
+
 );
 
-CREATE TABLE IF NOT EXSISTS employee(
-    id INT NOT NULL PRIMARY KEY,
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-
-    FOREIGN KEY (role_id),
-    REFERENCES role(id),
-
-    FOREIGN KEY (manager_id),
-    REFERENCES role(id),
-
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
